725fc5dafeba2bd774d8167bbb5338c96bc52539
denis.pushkarev <denis.pushkarev@corp.mail.ru>
2020-08-04 19:25:09 +0300

    fix

--- docker/composer.sh
+++ docker/composer.sh
@@ -3,4 +3,4 @@
 call="cd /app && /usr/local/bin/composer"
 
 eval "docker exec -it laravel_apache bash -c \"$call $*\""
-eval "docker exec -it laravel_apache bash -c \"chmod -R a+w /da/src/vendor/\""
+eval "docker exec -it laravel_apache bash -c \"chmod -R a+w /app/vendor/\""
