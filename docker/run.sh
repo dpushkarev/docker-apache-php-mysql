a89d59c3c5596c8b0a2079a77283921791babe04
denis.pushkarev <denis.pushkarev@corp.mail.ru>
2020-08-04 09:23:51 +0300

    add variables

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,1 @@
 docker-compose up -d --remove-orphans
-
-docker exec laravel_apache_con chown -R root:www-data /usr/local/apache2/logs
-docker exec laravel_php_con chown -R root:www-data /usr/local/etc/logs
